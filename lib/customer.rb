f92de1807bdc6655b202563bc3b6d92a1c45d7f2
Alice Freda <alicefreda@gmail.com>
2019-11-09 03:21:51 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -33,5 +33,18 @@ class Customer
     tip = total * 0.2
     Meal.new(waiter, self, total, tip)
   end
+  
+  def self.oldest_customer
+      oldest_age = 0
+      oldest_customer = nil
+      self.all.each do |customer|
+        if customer.age > oldest_age
+          oldest_age = customer.age
+          oldest_customer = customer
+        end
+      end
+      oldest_customer
+    end
+
 
 end
