c687393882a4f5f1708e46f0ecdbf5801111a0fa
Alice Freda <alicefreda@gmail.com>
2019-11-09 02:37:50 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,8 +1,14 @@
 class Waiter
-attr_reader :name, :yrs_experience
-@@all = []
+  attr_reader :name, :yrs_experience
+  @@all = []
 
-def initialize(name, exp)
-  
-end
+  def initialize(name, exp)
+    @name = name
+    @yrs_experience = exp
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
