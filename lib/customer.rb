36107a653891eb5e0fc322668be196769a02f599
Alice Freda <alicefreda@gmail.com>
2019-11-09 02:26:25 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,20 @@
 class Customer
-  
+  attr_reader :name, :age
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.customer == self}
+  end
+  def waiters
+
+  end
 end
